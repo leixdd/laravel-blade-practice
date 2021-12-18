0f63373c93f826cce5532759c21c3f92e8859463
Lei <lecias.ariel@gmail.com>
2021-12-18 08:25:22 +0800

    added link

--- resources/views/requests/create.blade.php
+++ resources/views/requests/create.blade.php
@@ -16,7 +16,7 @@
                 @foreach($certs as $cert)
                 <div class="group relative">
                     <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                        <img src="/img/{{ $cert->image_path }}" class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+                        <a href="/img/{{ $cert->image_path }}" target="_blank"><img src="/img/{{ $cert->image_path }}" class="w-full h-full object-center object-cover lg:w-full lg:h-full"></a>
                     </div>
                     <div class="mt-4 flex justify-between">
                         <div>
